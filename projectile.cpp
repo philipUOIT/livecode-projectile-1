0584a5254a37483f070d2e519d112040bf9fdd23
Philip Dos Santos <philip.dossantos@uoit.net>
2017-10-11 23:00:56 +0100

    complete code

--- projectile.cpp
+++ projectile.cpp
@@ -1,10 +1,19 @@
 #include "projectile.hpp"
 
 const double m = 1;
-const double g = 9.8066;
+const double q = 1;
+const double k = 0.5;
 const double dt = 0.001;
+const double e0 = 1;
+const double w = 2;
 
-auto force(TState s) { return VecR2<double>{0, -m * g}; }
+auto field(TState s){
+  return VecR2<double>{cos(k*s.position.z - w*s.t), sin(k*s.position.z - w*s.t), 0};
+}
+
+auto force(TState s) {
+   return field(s)*q;
+  }
 
 auto euler_step(TState s, VecR2<double> accel) {
   TState next;
@@ -12,7 +21,7 @@ auto euler_step(TState s, VecR2<double> accel) {
   next.position = s.position + (s.velocity * dt);
   next.velocity = s.velocity + (accel * dt);
   return next;
-  
+
 }
 
 void n_steps(unsigned n, TState state0) {
@@ -29,6 +38,6 @@ void n_steps(unsigned n, TState state0) {
 }
 
 int main() {
-  n_steps(1200, TState{0., {0, 0.1}, {5, 5}});
+  n_steps(1200, TState{0.0, {-e0/(pow(w,2)*pow(2.0,1.0/2.0)), 0,0}, {0, e0/(w*pow(2.0,1.0/2.0)) ,-0.25}});
   return 0;
 }
--- projectile.hpp
+++ projectile.hpp
@@ -1,21 +1,23 @@
 #pragma once
 
 #include <cstdio>
+#include <math.h>
 
 template <typename T>
 struct VecR2 {
   T x{0};
   T y{0};
+  T z{0};
 };
 
 template <typename T>
 auto operator+(VecR2<T> a, VecR2<T> b) {
-  return VecR2<T>{a.x + b.x, a.y + b.y};
+  return VecR2<T>{a.x + b.x, a.y + b.y, a.z + b.z};
 }
 
 template <typename T>
 auto operator*(VecR2<T> a, T c) {
-  return VecR2<T>{c * a.x, c * a.y};
+  return VecR2<T>{c * a.x, c * a.y, a.z * c};
 }
 
 template <typename T>
@@ -25,16 +27,16 @@ auto operator*(T c, VecR2<T> a) {
 
 template <typename T>
 auto operator/(VecR2<T> a, T c) {
-  return VecR2<T>{a.x / c, a.y / c};
+  return VecR2<T>{a.x / c, a.y / c, a.z / c};
 }
 
 struct TState {
   double t{0};
-  VecR2<double> position{0, 0};
-  VecR2<double> velocity{0, 0};
+  VecR2<double> position{0, 0, 0};
+  VecR2<double> velocity{0, 0, 0};
 };
 
 void print_tstate(TState s) {
-  printf("%f %f %f %f %f\n", s.t, s.position.x, s.position.y, s.velocity.x,
-         s.velocity.y);
+  printf("%f %f %f %f %f %f %f\n", s.t, s.position.x, s.position.y, s.position.z, s.velocity.x,
+         s.velocity.y, s.velocity.z);
 }
